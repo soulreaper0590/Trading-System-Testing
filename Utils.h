d6b4eac9dfc286cfeab0d606a403a837bbd756c6
Prakhar Thakuria <prakharthakuria1@gmail.com>
2026-07-17 20:37:51 +0800

    Refactor Message struct and add Trade struct

--- Utils.h
+++ Utils.h
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <string_view>
-#include <charconv>
-#include <vector>
-#include <optional>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 
 enum class RequestType : char {
@@ -21,22 +14,36 @@ enum class Side : char {
 };
 
 struct Message {
-    double price{0.0};
-    long int exchange_ticker{0};
-    long int order_id{0};
-    long int quantity{0};
+    ordPx_t px{0.0};
+    ExchangeTicker exchangeTicker{0};
+    ordId_t orderId{0};
+    ordSz_t sz{0};
     Side side{Side::Buy};
     RequestType request_type{RequestType::New};
 
     void print() const {
-        std::cout << "ExchangeId: " << exchange_ticker
+        std::cout << "ExchangeId: " << exchangeTicker
                   << " | Type: " << static_cast<char>(request_type)
-                  << " | ID: " << order_id
+                  << " | ID: " << orderId
                   << " | Side: " << static_cast<char>(side)
-                  << " | Qty: " << quantity
-                  << " | Price: " << price << "\n";
+                  << " | Qty: " << sz
+                  << " | Price: " << px << "\n";
     }
 };
+using ordId_t = long int;  // exchange order id
+using ordPx_t = double;    // price
+using ordSz_t = long int;  // quantity
+using ExchangeTicker = long long int;
 
+inline char sideChar(Side aSide) {
+    return aSide == Side::Buy ? 'B' : 'S';
+}
 
-
+// for some reason we need to print this twice
+struct Trade {
+    ordId_t takerId;   // incoming / aggressing order
+    ordId_t makerId;   // resting order that was hit
+    ordPx_t px;        // execution price (maker price)
+    ordSz_t sz;        // executed quantity
+    Side    takerSide; // side of the aggressor
+};
